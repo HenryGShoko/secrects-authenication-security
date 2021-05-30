58c48454c4a1a5b6035f3f8faf64d33191a266f4
HenryGShoko <gshokx91@gmail.com>
2021-05-30 17:03:46 +0200

    Regiter users with username and password

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//jshint esversion:6
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -10,6 +10,15 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true})
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model('User', userSchema)
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -21,6 +30,38 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', (req, res) => {
+const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+})
+
+newUser.save((err) => {
+    if (err) {
+        console.log(err)
+    }else {
+        res.render('secrets')
+    }
+})
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({ email: username},(err, foundUser) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (foundUser){
+                if(foundUser.password === password){
+                    res.render('secrets')
+                }
+            }
+        }
+    })
 })
 
 
